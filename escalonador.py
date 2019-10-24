d037e270d658773084e62feef6b6cd2c2aafd958
Enzo <enzo.ditaliano@gmail.com>
2019-10-23 21:10:37 -0300

    Começo do carregamento de informações dos processos

--- escalonador.py
+++ escalonador.py
@@ -1,7 +1,13 @@
-class BCP:
+import sys
+
+class BCP(object):
     def __init__(self):
         self.pid = 0
         self.prioridade = 0
         self.estado = 0
-        self.tempos = 0
+        self.tempos = []
+        self.eventosIO = []
 
+end_condition = 0
+
+while not end_condition:
